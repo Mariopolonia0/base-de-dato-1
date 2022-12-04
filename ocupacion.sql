2c42a75e3f14816586f9259f3a3d4c49ba28a589
Mario Peña Polonia <65502311+Mariopolonia0@users.noreply.github.com>
2022-12-04 15:47:47 -0400

    Create ocupacion.sql
    agregando consulta de ocupaciones

---
+++ ocupacion.sql
@@ -0,0 +1,32 @@
+//crear tabla de libros
+create table Books(
+	bookId int primary key not null,
+	nombre varchar not null,
+	nombreAutor varchar not null,
+	edicion varchar not null,
+	precio real not null
+)
+
+
+INSERT INTO Ocupaciones 
+(ocupacionId, descripcion,fechaRegistro,Salario)
+
+VALUES
+ (2, "Diseñador","16/6/2022 0:0:0",90000 ),
+ (3, "Cocinero","16/6/2022",90000 ),
+ (4, "Ingeniero Civil","16/6/2022",90000 ),
+ (5, "Ingeniero Sitema","16/6/2022",90000 ),
+ (6, "Abogado","16/6/2022",60000 ),
+ (7, "Ladron","16/6/2022",90000 ),
+ (8, "Politico","16/6/2022",90000 ),
+ (9, "Diseñador Grafico","16/6/2022",90000 ),
+ (10, "Trailero","16/6/2022",90000 ),
+ (11, "Mecanico","16/6/2022",90000 ),
+ (12, "Artista","16/6/2022",90000 ),
+ (13, "Maestro","16/6/2022",90000 )
+
+INSERT INTO ocupaciones 
+(OcupacionId, Descripcion,FechaRegistro,Salario)
+VALUES
+ (14, "Diseñador UX","2022-08-12 0:0:0.0",11000 ),
+ (15, "Diseñador UI","2022-08-12 0:0:0.0",11000 )
