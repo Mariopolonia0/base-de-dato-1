a38d42fa425d8c03b2ec9c01e885f59ea80401a5
Mario Peña Polonia <65502311+Mariopolonia0@users.noreply.github.com>
2023-02-13 22:38:21 -0400

    Update practicaInHackerRank.sql

--- practicaInHackerRank.sql
+++ practicaInHackerRank.sql
@@ -9,6 +9,7 @@ SELECT * FROM CITY
 WHERE POPULATION > 100000 AND COUNTRYCODE = 'USA'
 
 /*
+ * EJERCICIO 2
  * Query the NAME field for all American cities in the CITY table with populations larger than 120000.
  * The CountryCode for America is USA.
  * The CITY table is described as follows:
