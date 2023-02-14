c64bf1fd0eb43bafd9e09795bddb0d521cfc8ead
Mario Peña Polonia <65502311+Mariopolonia0@users.noreply.github.com>
2023-02-13 22:37:49 -0400

    Update practicaInHackerRank.sql

--- practicaInHackerRank.sql
+++ practicaInHackerRank.sql
@@ -1,8 +1,18 @@
 /*
-Query all columns for all American cities in the CITY table with populations larger than 100000.
-The CountryCode for America is USA.
-The CITY table is described as follows:
-*/
+ * EJERCICIO 1
+ * Query all columns for all American cities in the CITY table with populations larger than 100000.
+ * The CountryCode for America is USA.
+ * The CITY table is described as follows:
+ */
 
 SELECT * FROM CITY
 WHERE POPULATION > 100000 AND COUNTRYCODE = 'USA'
+
+/*
+ * Query the NAME field for all American cities in the CITY table with populations larger than 120000.
+ * The CountryCode for America is USA.
+ * The CITY table is described as follows:
+ */
+
+SELECT NAME FROM CITY
+WHERE POPULATION > 120000 AND COUNTRYCODE = 'USA'
